d011a9db439eab79237f1d13273ba55334ba6a52
mledda17 <m.ledda45@studenti.unica.it>
2024-04-02 10:38:38 +0200

    Commit ISDE Project

---
+++ app/routes/__init__.py
@@ -0,0 +1,7 @@
+from .classifications import *
+from .classifications_id import *
+from .info import *
+from .home import *
+from .transformation import *
+from .histogram import *
+from .remove_image import *
---
+++ app/routes/home.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+from config import Configuration
+
+conf = Configuration()
+
+
+@app.route('/home', methods=['GET'])
+@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
